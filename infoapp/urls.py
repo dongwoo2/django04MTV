cc94ef9585edaed6d34a5c410a0a60b9c7a39ecc
dongwoo2 <ieiedong@naver.com>
2024-03-19 23:57:45 +0900

    20240319 detail 부분 생성완료 수정하기 삭제하기 만들기 예정

--- infoapp/urls.py
+++ infoapp/urls.py
@@ -6,4 +6,6 @@ app_name = 'infoapp'
 urlpatterns = [
     path('', views.index, name='index'),
     path('add/', views.add, name='add'),
+    path('detail/', views.detail1, name = 'detail1'),
+    path('detail/<int:id>/', views.detail2, name = 'detail2'),
 ]
--- infoapp/views.py
+++ infoapp/views.py
@@ -23,3 +23,21 @@ def add(request):
         intro = request.POST['intro']
         Info(name=name, age=age, intro=intro).save()
         return HttpResponseRedirect('/info/')
+    
+    
+def detail1(request): # 얘는 파라미터로 받아서 처리하는 방식
+    id = request.GET['id']
+    info = Info.objects.get(id=id)
+    return render(
+        request,
+        'infoapp/detail.html',
+        {'info':info}
+    )
+    
+def detail2(request, id): # 경로변수를 사용하는 방식 request말고도 경로에 있는 id도 뽑아올 수 있게 id 매개변수 설정
+    info = Info.objects.get(id=id)
+    return render(
+        request,
+        'infoapp/detail.html',
+        {'info':info}
+    )
